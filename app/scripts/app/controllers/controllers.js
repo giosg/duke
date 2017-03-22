15df324e84a5fc8fbd8342c298276b53926accef
Joona Pääkkönen <joona@giosg.com>
2017-03-22 14:47:36 +0200

    Bump version

--- app/scripts/app/controllers/controllers.js
+++ app/scripts/app/controllers/controllers.js
@@ -4,7 +4,7 @@
    angular.module('popup')
   .controller('OverViewController', ['$scope', 'clientInfo', 'ClientInfoService', 'PortService',
     function($scope, clientInfo, ClientInfoService, PortService) {
-      $scope.currentVersion = "0.3.0";
+      $scope.currentVersion = "0.3.1";
       $scope.clientInfo = clientInfo;
       $scope.enableCobrowse = function() {
         ClientInfoService.enableCobrowse();
